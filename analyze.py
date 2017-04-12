e432b1ca4068c079ebbd87a9f20d98bae0cbf22f
Scott McAuley <smcauley@gmail.com>
2017-04-12 19:59:28 -0400

    Add ability to select and plot multiple chromatograms. Add ability to output text to GUI while uploading files.

--- analyze.py
+++ analyze.py
@@ -12,21 +12,12 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
 
-class Meta:
-    def __init__(self, field, term):
-        self.metadata = []
-        self.client = MongoClient()
-        self.db = self.client['nodbase']
-        self.cursor = self.db['meta'].find({field: term})
-
-        self.expID = []
-
-        for self.document in self.cursor:
-            self.metadata.append(self.document)
-
-
 class Chromatogram:
     def __init__(self, expID):
+        self.find_chrom(expID)
+        self.plot()
+
+    def find_chrom(self, expID):
         self.chrom = {}
         self.client = MongoClient()
         self.db = self.client['nodbase']
@@ -39,52 +30,141 @@ class Chromatogram:
                  'totion': self.document['totion']})
                #  'ms_level': self.document['ms_level']}) Need to add this post testing
 
+    def plot(self):
+        self.win = tk.Toplevel()
+        self.win.title("Search results")
+
+        self.f = Figure(figsize=(5, 4), dpi=100)
+        self.a = self.f.add_subplot(111)
+        self.x = self.chrom['retentionTime']
+        self.y = self.chrom['bpi']
+
+        self.a.plot(self.x, self.y)
+
+        # a tk.DrawingArea
+        self.canvas = FigureCanvasTkAgg(self.f, master=self.win)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.toolbar = NavigationToolbar2TkAgg(self.canvas, self.win)
+        self.toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        def on_key_event(event):
+            print('you pressed %s' % event.key)
+            key_press_handler(event, self.canvas, self.toolbar)
+
+        self.canvas.mpl_connect('key_press_event', on_key_event)
+
+        self.button = tk.Button(master=self.win, text='Quit', command=self.win.destroy)
+        self.button.pack(side=tk.BOTTOM)
+
+
+class Analyze:
+    def create_window(self):
+        self.win = tk.Toplevel()
+        self.win.title("Search and Analyze Nodbase Data")
+
+        self.top_labels = tk.Frame(self.win)
+        self.top_labels.pack()
+
+        tk.Label(self.top_labels, width=10, text="Search field").pack(side='left')
+        tk.Label(self.top_labels, width=10, text="Search term").pack(side='right')
+
+        self.entries = []
+
+        self.top_entries = tk.Frame(self.win)
+        self.top_entries.pack()
+
+        self.ent1 = tk.Entry(self.top_entries)
+        self.ent1.pack(side='left')
+        self.ent2 = tk.Entry(self.top_entries)
+        self.ent2.pack(side='right')
+
+        self.entries.append((self.ent1, self.ent2))
+
+        self.bottom_frame = tk.Frame(self.win)
+        self.bottom_frame.pack()
+
+        self.btn_upload = tk.Button(self.bottom_frame, text='Search', width=7, command=self.search)
+        self.btn_upload.pack(side='left')
+        self.btn_exit = tk.Button(self.bottom_frame, text='Exit', width=7, command=self.win.destroy)
+        self.btn_exit.pack(side='right')
+
+    def search(self):
+        # This assumes only a single field:term pair
+        for self.entry in self.entries:
+            field = self.entry[0].get()
+            term = self.entry[1].get()
+
+        # Need to search across the metadata and allow for selection of hits
+        # Place those hits into separate Meta instances
+        self.metadata = []
+        self.client = MongoClient()
+        self.db = self.client['nodbase']
+        self.cursor = self.db['meta'].find({field: term})
+
+        for self.document in self.cursor:
+            self.metadata.append(self.document)
+
+        # Show the number of results
+        self.res_frame = tk.Frame(self.win)
+        self.res_frame.pack()
+        self.res_count1 = tk.Label(self.res_frame, width=20, text="Number of results")
+        self.res_count2 = tk.Label(self.res_frame, width=20, text=len(self.metadata))
+        self.res_count1.pack(side='left')
+        self.res_count2.pack(side='right')
+
+        # Define the Result headers
+        self.res_head = tk.Frame(self.win)
+        self.res_head.pack()
+        self.res_head1 = tk.Label(self.res_head, width=20, text="Selection")
+        self.res_head2 = tk.Label(self.res_head, width=20, text="Date")
+        self.res_head3 = tk.Label(self.res_head, width=20, text="Strain")
+        self.res_head4 = tk.Label(self.res_head, width=20, text="Experiment ID")
+        self.res_head1.pack(side='left')
+        self.res_head2.pack(side='left')
+        self.res_head3.pack(side='left')
+        self.res_head4.pack(side='left')
+
+        # Variable for selecting the results you want to test
+        self.var = {}
+
+        for i, self.result in enumerate(self.metadata):
+            self.var[self.result['_id']] = tk.Variable()
+            self.res = tk.Frame(self.win)
+            self.res.pack()
+            self.res_check = tk.Checkbutton(self.res,
+                                            width=5,
+                                            variable=self.var[self.result['_id']],
+                                            onvalue=1, offvalue=0,
+                                            text="")
+            self.res1 = tk.Label(self.res, width=20, text=self.result["date"])
+            self.res2 = tk.Label(self.res, width=20, text=self.result["strain"])
+            self.res3 = tk.Label(self.res, width=40, text=self.result["_id"])
+            self.res_check.pack(side='left')
+            self.res1.pack(side='left')
+            self.res2.pack(side='left')
+            self.res3.pack(side='left')
+
+        # Selection button
+        self.btn_select = tk.Frame(self.win)
+        self.btn_select.pack()
+        self.btn_select1 = tk.Button(self.btn_select, text='Select', width=7, command=self.find_chrom)
+        self.btn_select1.pack()
+
+    def find_chrom(self):
+        # Locates the experiment IDs from the selection and saves them to expID
+        self.chromatos = {}
+        for self.id in self.var:
+            if self.var[self.id].get() == 1:
+                self.chromatos[self.id] = Chromatogram(self.id)
+
+    def __init__(self):
+        self.create_window()
+
 
 def analyze():
-    win = tk.Toplevel()
-    win.title("Analyze Nodbase Data")
-
-
-    message = "This will have the search and analysis functions"
-    tk.Label(win, text=message).pack()
-    b1 = tk.Button(win, text='Done', command=search)
-    b1.pack(side=tk.LEFT, padx=5, pady=5)
-    b2 = tk.Button(win, text='Quit', command=win.destroy)
-    b2.pack(side=tk.LEFT, padx=5, pady=5)
-
-
-def search():
-    met = Meta('strain', 'SA237')
-    chrom = Chromatogram(met.metadata[3]['_id'])
-    print(met.metadata)
-    print(chrom.chrom)
-
-    win = tk.Toplevel()
-    win.title("Search results")
-
-    f = Figure(figsize=(5, 4), dpi=100)
-    a = f.add_subplot(111)
-    x = chrom.chrom['retentionTime']
-    y = chrom.chrom['bpi']
-
-    a.plot(x, y)
-
-    # a tk.DrawingArea
-    canvas = FigureCanvasTkAgg(f, master=win)
-    canvas.show()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    toolbar = NavigationToolbar2TkAgg(canvas, win)
-    toolbar.update()
-    canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    def on_key_event(event):
-        print('you pressed %s' % event.key)
-        key_press_handler(event, canvas, toolbar)
-
-    canvas.mpl_connect('key_press_event', on_key_event)
-
-    button = tk.Button(master=win, text='Quit', command=win.destroy)
-    button.pack(side=tk.BOTTOM)
+    Analyze()
 
 
--- upload.py
+++ upload.py
@@ -3,156 +3,165 @@ from pyteomics import mzxml  # For reading in XMl data
 from pymongo import MongoClient  # For connecting to MongoDB
 
 
+class Upload:
+    def upload(self):
+        self.win = tk.Toplevel()
+        self.win.title("Upload mzXML Data to Nodbase")
+
+        self.fields = ['Filename',
+                       'Name',
+                       'Strain ID',
+                       'Growth Media',
+                       'Growth Method (solid/liquid)',
+                       'Illicitor Name',
+                       'Illicitor Concentration (uM)',
+                       'Extraction Solvent',
+                       'Column Type',
+                       'Experiment Date',
+                       'Upload Date']
+
+        # Establish form for inserting meta data
+        self.entries = []
+        for self.field in self.fields:
+            self.row = tk.Frame(self.win)
+            self.lab = tk.Label(self.row, width=28, text=self.field, anchor='w')
+            self.ent = tk.Entry(self.row)
+            self.row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+            self.lab.pack(side=tk.LEFT)
+            self.ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+            self.entries.append((self.field, self.ent))
+
+        self.text_output = tk.Frame(self.win)
+        self.output = tk.Text(self.text_output)
+        self.text_output.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        self.output.pack()
+
+        self.b1 = tk.Button(self.win, text='Enter',
+                            command=self.fetch)
+        self.b1.pack(side=tk.LEFT, padx=5, pady=5)
+        self.b2 = tk.Button(self.win, text='Quit', command=self.win.destroy)
+        self.b2.pack(side=tk.LEFT, padx=5, pady=5)
+
+    # No idea why the callback text is only shown if the function throws an error
+    def callback(self, s):
+        print(s)
+        self.output.insert(tk.INSERT, s+'\n')
+        self.output.pack()
+        self.win.update_idletasks()
+
+    def fetch(self):
+        """
+        Transform data from the GUI into a dictionary for the extract function
+        :param entries: the inputs from the GUI
+        :return: NA
+        """
+
+        self.callback('Extraction beginning...')
+
+        self.meta = {}
+        for self.entry in self.entries:
+            self.field = self.entry[0]
+            self.text = self.entry[1].get()
+
+            self.meta[self.field] = self.text
+
+        """
+        Processing the chromatogram, MS and MS^2 data the mzXML file and inputting to MongoDB
+        :param meta: The inputted metadata for the experiment
+        """
+        # Establish MongoDB connection
+        self.client = MongoClient()
+        self.db = self.client['nodbase']
+
+        # Create an iterable from the mzxml file
+        self.file = mzxml.read(self.meta['Filename'])
+
+        self.retentionTime = []
+        self.bpi = []
+        self.totion = []
+
+        # Insert Metadata into the database and obtain unique ID
+        self.id = self.db["meta"].insert_one(self.meta)
+        self.expID = self.id.inserted_id
+
+        self.callback("Metadata insertion complete...")
+
+    # TODO: May want to have both chromatograms pulled in the same iterable run?
+        # Chromatogram - MS Level 1
+        for self.scan in self.file:
+            if self.scan['msLevel'] == 1:
+                self.retentionTime.append(self.scan["retentionTime"])
+                self.bpi.append(self.scan["basePeakIntensity"])
+                self.totion.append(self.scan["totIonCurrent"])
+
+        self.chrom = {"retentionTime": self.retentionTime,
+                      "bpi": self.bpi,
+                      "totion": self.totion,
+                      "ms_level": 1,
+                      "expID": self.expID}
+        self.callback("MS Chromatogram extraction complete...")
+
+        # Add the chromatogram to the database
+        self.db["chrom"].insert_one(self.chrom)
+        self.callback("MS Chromatogram insertion complete...")
+
+        # Remove the chromatogram variable
+        del self.chrom
+
+        # Re-establish iterable file and the holding variables
+        self.file = mzxml.read(self.meta['Filename'])
+        self.retentionTime = []
+        self.bpi = []
+        self.totion = []
+
+        # Chromatogram - MS Level 2
+        for self.scan in self.file:
+            if self.scan['msLevel'] == 2:
+                self.retentionTime.append(self.scan["retentionTime"])
+                self.bpi.append(self.scan["basePeakIntensity"])
+                self.totion.append(self.scan["totIonCurrent"])
+
+        self.chrom = {"retentionTime": self.retentionTime,
+                 "bpi": self.bpi,
+                 "totion": self.totion,
+                 "ms_level": 2,
+                 "expID": self.expID}
+        self.callback("MS^2 Chromatogram extraction complete...")
+
+        # Add the chromatogram to the database
+        self.db["chrom"].insert_one(self.chrom)
+        self.callback("MS^2 Chromatogram insertion complete...")
+
+        # Remove the chromatogram variable
+        del self.chrom
+        del self.bpi
+        del self.retentionTime
+        del self.totion
+
+        # Re-establish iterable file
+        self.file = mzxml.read(self.meta['Filename'])
+
+        # MS Spectra
+        for self.scan in self.file:
+            self.spectrum = {"expID": self.expID,
+                        "retentionTime": self.scan["retentionTime"],
+                        "m/z": self.scan["m/z array"].tolist(),
+                        "intensity": self.scan["intensity array"].tolist()}
+
+            if self.scan['msLevel'] == 1:
+                self.spectrum['ms_level'] = 1
+                self.db["ms"].insert_one(self.spectrum)
+
+            elif self.scan['msLevel'] == 2:
+                self.spectrum['ms_level'] = 2
+                self.db["ms2"].insert_one(self.spectrum)
+
+        self.callback("MS extraction and insertion complete...")
+        self.callback('Thank you for adding your data to Nodbase')
+
+    def __init__(self):
+        self.upload()
+
+
 def upload():
-    win = tk.Toplevel()
-    win.title("Upload mzXML Data to Nodbase")
-
-    fields = ['Filename',
-              'Name',
-              'String ID',
-              'Growth Media',
-              'Growth Method (solid/liquid)',
-              'Illicitor Name',
-              'Illicitor Concentration (uM)',
-              'Extraction Solvent',
-              'Column Type',
-              'Experiment Date',
-              'Upload Date']
-
-    # Establish form for inserting meta data
-    entries = []
-    for field in fields:
-        row = tk.Frame(win)
-        lab = tk.Label(row, width=28, text=field, anchor='w')
-        ent = tk.Entry(row)
-        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
-        lab.pack(side=tk.LEFT)
-        ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
-        entries.append((field, ent))
-
-    row = tk.Frame(win)
-    output = tk.Text(row)
-    row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
-    output.pack(side=tk.LEFT)
-    b1 = tk.Button(win, text='Enter',
-                   command=lambda: fetch(entries, output))
-    b1.pack(side=tk.LEFT, padx=5, pady=5)
-    b2 = tk.Button(win, text='Quit', command=win.destroy)
-    b2.pack(side=tk.LEFT, padx=5, pady=5)
-
-# No idea why the callback text is only shown if the function throws an error
-def callback(s, output):
-    print("call back")
-    output.insert(tk.END, s)
-    output.see(tk.END)
-    output.pack()
-
-
-def fetch(entries, output):
-    callback('Extraction beginning...', output)
-    """
-    Transform data from the GUI into a dictionary for the extract function
-    :param entries: the inputs from the GUI
-    :return: NA
-    """
-    meta = {}
-    for entry in entries:
-        field = entry[0]
-        text = entry[1].get()
-
-        meta[field] = text
-
-    """
-    Processing the chromatogram, MS and MS^2 data the mzXML file and inputting to MongoDB
-    :param meta: The inputted metadata for the experiment
-    """
-    # Establish MongoDB connection
-    client = MongoClient()
-    db = client['nodbase']
-
-    # Create an iterable from the mzxml file
-    print(meta)
-    file = mzxml.read(meta['Filename'])
-
-    retentionTime = []
-    bpi = []
-    totion = []
-
-    # Insert Metadata into the database and obtain unique ID
-    id = db["meta"].insert_one(meta)
-    expID = id.inserted_id
-    print("Metadata insertion complete...")
-    output.insert(tk.END, "Metadata insertion complete...")
-    output.see(tk.END)
-
-# TODO: May want to have both chromatograms pulled in the same iterable run?
-    # Chromatogram - MS Level 1
-    for scan in file:
-        if scan['msLevel'] == 1:
-            retentionTime.append(scan["retentionTime"])
-            bpi.append(scan["basePeakIntensity"])
-            totion.append(scan["totIonCurrent"])
-
-    chrom = {"retentionTime": retentionTime,
-              "bpi": bpi,
-              "totion": totion,
-              "ms_level": 1,
-             "expID": expID}
-    print("Chromatogram extraction complete...")
-
-    # Add the chromatogram to the database
-    db["chrom"].insert_one(chrom)
-    print("Chromatogram insertion complete...")
-
-    # Remove the chromatogram variable
-    del chrom
-
-    # Re-establish iterable file and the holding variables
-    file = mzxml.read(meta['Filename'])
-    retentionTime = []
-    bpi = []
-    totion = []
-
-    # Chromatogram - MS Level 2
-    for scan in file:
-        if scan['msLevel'] == 2:
-            retentionTime.append(scan["retentionTime"])
-            bpi.append(scan["basePeakIntensity"])
-            totion.append(scan["totIonCurrent"])
-
-    chrom = {"retentionTime": retentionTime,
-             "bpi": bpi,
-             "totion": totion,
-             "ms_level": 2,
-             "expID": expID}
-    print("Chromatogram extraction complete...")
-
-    # Add the chromatogram to the database
-    db["chrom"].insert_one(chrom)
-    print("Chromatogram insertion complete...")
-
-    # Remove the chromatogram variable
-    del chrom
-    del bpi
-    del retentionTime
-    del totion
-
-    # Re-establish iterable file
-    file = mzxml.read(meta['Filename'])
-
-    # MS Spectra
-    for scan in file:
-        spectrum = {"expID": expID,
-                    "retentionTime": scan["retentionTime"],
-                    "m/z": scan["m/z array"].tolist(),
-                    "intensity": scan["intensity array"].tolist()}
-
-        if scan['msLevel'] == 1:
-            spectrum['ms_level'] = 1
-            db["ms"].insert_one(spectrum)
-
-        elif scan['msLevel'] == 2:
-            spectrum['ms_level'] = 2
-            db["ms2"].insert_one(spectrum)
-
-    print("MS extraction and insertion complete...")
+    Upload()
